bdf859368f46d42bf7c9f3a7750468ca36672b47
Simon Kreuzer <mail@monsi.org>
2020-11-07 12:25:01 +0100

    fixed issue with english locale

--- discord_birthday_bot/date_util.py
+++ discord_birthday_bot/date_util.py
@@ -6,26 +6,29 @@ from babel.dates import format_date, get_date_format
 
 from discord_birthday_bot import config_util as config
 
-lang = config.get_language()
-if len(lang) == 2:
-    lang += '_' + lang.upper()
-
-LOCALE = lang + '.utf8'
-
 NO_YEAR = 1
+
+STANDARD_LANG = 'en_US'
+
+lang = config.get_language()
 
 # Set locale #
 try:
-    locale.setlocale(locale.LC_ALL, LOCALE)  # set locale
+    locale.setlocale(locale.LC_ALL, lang + '.utf8')  # set locale
 except locale.Error:
-    print('Error: Locale %s not existing.' % LOCALE)
-    LOCALE = 'en_US.utf8'
-    locale.setlocale(locale.LC_ALL, LOCALE)
+    print('Error: Locale %s not existing.' % lang)
+    lang = STANDARD_LANG
+    locale.setlocale(locale.LC_ALL, lang + '.utf8')
+
+
+def get_current_locale():
+    """Returns the current locale string without the '.utf8'."""
+    return locale.getlocale(locale.LC_ALL)[0]
 
 
 def parse_to_date(msg):
     """Parses a date string and returns a datetime object."""
-    date = dateparser.parse(msg, locales=[LOCALE[:2]])  # parse date
+    date = dateparser.parse(msg, locales=[get_current_locale()[:2]])  # parse date
     # If is current year, set year to 1 #
     if is_current_year(date):
         date = date.replace(year=NO_YEAR)
@@ -34,10 +37,10 @@ def parse_to_date(msg):
 
 def parse_to_string(date):
     """Formats the date according to the current locale and the existence of the year."""
-    date_format = get_date_format(format='long', locale=lang).pattern
+    date_format = get_date_format(format='long', locale=get_current_locale()).pattern
     if not has_year(date):
         date_format = date_format.replace('y', '').replace(',', '').rstrip()
-    return format_date(date, format=date_format, locale=lang)
+    return format_date(date, format=date_format, locale=get_current_locale())
 
 
 def has_year(date):
